7f36eafa67e50b431b1601c1e2b0af92698ac3b6
Karim Idrisov <Karimm19@mail.ru>
2024-02-08 16:24:36 +0500

    Домашка №14 переделана.

--- src/main/java/ru/idrisov/homeworks/homework14/Doma14.java
+++ src/main/java/ru/idrisov/homeworks/homework14/Doma14.java
@@ -1,15 +1,17 @@
 package ru.idrisov.homeworks.homework14;
 
+import java.util.Arrays;
+
 public class Doma14 {
 
     public static void main(String[] args) {
 
         Doma14 main = new Doma14(); // Создаем объект для даль использования в текущем классе
-        String[][] corrects = { {"1", "2", "3", "4"},{"5", "6", "7","8"}, {"9", "5", "6","5"}, {"5", "4", "3", "2"}};
-        String[][] correc = { {"1", "2", "2", "4"},{"5", "й", "7","8"}, {"9", "5", "6","5"}, {"5", "4", "3", "2"}};
+        String[][] corrects = {{"1", "2", "3", "4"}, {"5", "6", "й", "8"}, {"9", "5", "6", "5"}, {"5", "4", "3", "2"}};
+        String[][] corrects2 = {{"1", "2", "2", "4"}, {"5", "q", "7", "8"}, {"9", "5", "2", "5"}, {"5", "4", "3", "2"}};
         try {
-            main.stringExceptionTest(corrects); //в main обращаемся к объекту передавая данные из массива
-            main.stringSumm(correc);
+            main.stringExceptionTest(corrects);
+            main.stringSumm(corrects2);
         } catch (AppArraySizeException e) {
             e.printStackTrace();
         } catch (AppArrayDataException ex) {
@@ -32,11 +34,22 @@ public class Doma14 {
         int sum = 0;
         for (int i = 0; i < correct.length; i++) {
             for (int j = 0; j < correct[i].length; j++) {
-               sum += Integer.parseInt(correct[i][j]);
+                if (isNumeric(correct[i][j]) == false)
+                    throw new AppArrayDataException("В ячейке " + Arrays.asList(correct[j]) + " лежат неверные данные");
+                sum += Integer.parseInt(correct[i][j]);
                 System.out.println(sum);
             }
         }
-       System.out.println("Сумма сложения чисел равна " + sum);
+        System.out.println("Сумма сложения чисел равна " + sum);
+    }
+
+    public static boolean isNumeric(String error) {
+        try {
+            Integer.parseInt(error);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
 
