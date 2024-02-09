89569e6396f389111a7d7f2bf61219f9593e4bd1
Karim Idrisov <Karimm19@mail.ru>
2024-02-09 11:49:00 +0500

    Домашка №16 переделана.

--- src/main/java/ru/idrisov/homeworks/homework16/PhoneBook.java
+++ src/main/java/ru/idrisov/homeworks/homework16/PhoneBook.java
@@ -3,36 +3,33 @@ package ru.idrisov.homeworks.homework16;
 
 import java.util.HashMap;
 import java.util.Map;
+
 public class PhoneBook {
 
+    private HashMap<String, String> book;
+
+    public PhoneBook() {
+        this.book = new HashMap<>();
+    }
+
     public void add(String fio, String phone) {
-        System.out.println("ФИО: " + fio + " Телефон: " + phone);
-    }
-    public Map<String, String> find(String fio) {
-        Map<String, String> book = new HashMap<>();
-        book.put("2222222", "fio");
-        book.put("33333333", "fio2");
-        book.put("111111111", "fio");
-        for (var entry : book.entrySet()) {
-            if (entry.getValue().equals(fio)) {
-                System.out.println("Поиск по имени " + fio + " определил принадлежность номер телефона " + entry.getKey());
-            }
-        }
-        return book;
+        book.put(fio, phone);
     }
 
-    public Map<String, String> containsPhoneNumber(String fio) {
-        Map<String, String> book = new HashMap<>();
-        book.put("fio", "2121122");
-        book.put("fio2", "232232323");
-        book.put("fio3", "232232323");
-        for (var entry : book.entrySet()) {
-            if (entry.getValue().equals(fio)) {
-                System.out.println("Поиск по телефону " + fio + " данный номер принадлежит " + entry.getKey());
-            }
-        }
-        return book;
+    public String find(String fio) {
+        String phoneNumber = book.get(fio);
+        if (phoneNumber == null) return "Номер не найден";
+        return phoneNumber;
     }
 
+    public String containsPhoneNumber(String number) {
+        String phoneNumber = "Информация не найдена";
+        for (Map.Entry<String, String> item : book.entrySet()) {
+            if (item.getValue().equals(number)) {
+                phoneNumber = "Номер телефона " + number + " найден. Принадлежит " + item.getKey();
+            }
+        }
+        return phoneNumber;
+    }
 
 }
