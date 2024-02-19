9c05c60b4028b2a2c0274570b00de18673f03477
Karim Idrisov <Karimm19@mail.ru>
2024-02-19 15:19:27 +0500

    Домашка №19 переделана.

--- src/main/java/ru/idrisov/homeworks/homework19/Doma19.java
+++ src/main/java/ru/idrisov/homeworks/homework19/Doma19.java
@@ -1,20 +1,25 @@
 package ru.idrisov.homeworks.homework19;
 
 
+import ru.idrisov.homeworks.homework14.AppArraySizeException;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 import java.io.IOException;
-
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Doma19 {
 
     public static void main(String[] args) throws IOException {
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
 
-        File file = new File("C:\\Users\\User\\Desktop\\OTUS-basic-homeworks\\src\\test\\java\\");
+        File file = new File(s + "\\src\\test\\java\\");
 
-        if (file.exists()) {// проверяем наличие файла
-            
+        if (file.exists()) {
+
             File[] arr = file.listFiles();
             for (int i = 0; i < arr.length; i++) {
                 System.out.println(arr[i].getName());
@@ -28,6 +33,7 @@ public class Doma19 {
             for (int i = 0; i < arr.length; i++) {
                 if (files.equals(arr[i].getName())) {
                     result = "Отлично! Файл под названием " + files + " найден!\nСодержимое файла: ";
+                    System.out.println(result);
                     break;
                 }
             }
@@ -37,34 +43,32 @@ public class Doma19 {
                 return;
             }
 
-            System.out.println(result);
-            try (BufferedInputStream in = new BufferedInputStream(new FileInputStream("C:\\Users\\User\\Desktop\\OTUS-basic-homeworks\\src\\test\\java\\" + files))) {
+            try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(s + "\\src\\test\\java\\" + files))) {
                 int n = in.read();
                 while (n != -1) {
                     System.out.print((char) n);
                     n = in.read();
                 }
+                Scanner sc = new Scanner(System.in);
+                System.out.println("\nНапишите любой текст для изменения содержимого в текущем файле: ");
+                String text = sc.nextLine();
+
+                if (text.isEmpty()) {
+                    System.out.println("Введите текст");
+                    return;
+                }
+
+                try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(s + "\\src\\test\\java\\" + files))) {
+                    byte[] buffer = text.getBytes(StandardCharsets.UTF_8);
+                    System.out.println("Вы успешно изменили содержимое файла " + files + " на " + text);
+                    out.write(buffer);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-            Scanner sc = new Scanner(System.in);
-            System.out.println("\nНапишите любой текст для изменения содержимого в текущем файле: ");
-            String text = sc.nextLine();
-
-            if (text.isEmpty()) {
-                System.out.println("Введите текст");
-                return;
-            }
-
-            System.out.println("Вы успешно изменили содержимое файла " + files + " на " + text);
-            try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("C:\\Users\\User\\Desktop\\OTUS-basic-homeworks\\src\\test\\java\\" + files))) {
-                byte[] buffer = text.getBytes(StandardCharsets.UTF_8);
-                out.write(buffer);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
 
         }
 
