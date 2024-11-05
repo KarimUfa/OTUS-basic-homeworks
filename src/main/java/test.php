25305951221963e596f4c2eff0b07ffea8798f0d
Karim Idrisov <Karimm19@mail.ru>
2024-11-05 19:33:11 +0500

    Задача 1

---
+++ src/main/java/test.php
@@ -0,0 +1,63 @@
+<?
+
+// Задача №1
+
+function postApi($text,$url){ //отправляем запрос на внешний API и получаем данные
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($text));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
+
+function getApp ($event_id,$event_date,$ticket_adult_price,$ticket_adult_quantity,$ticket_kid_price,$ticket_kid_quantity){
+global $mysql;
+
+$barcode = mt_rand(111111111111111111,99999999999999999999);
+$postbook = array("event_id" => $event_id, "event_date" => $event_date,"ticket_adult_price" => $ticket_adult_price,"ticket_adult_quantity" => $ticket_adult_quantity,"ticket_kid_price" => $ticket_kid_price,"ticket_kid_quantity" => $ticket_kid_quantity,"barcode" => $barcode);
+$equal_price = $ticket_adult_price + $ticket_kid_price; //общая стоимость билетов
+$urlbook = "https://api.site.com/book";
+$urlapprove = "https://api.site.com/approve";
+
+$data = json_decode(postApi($postbook,$urlbook), true);
+
+if($data['message'] == 'order successfully booked'){ // успешное принятие
+
+    $goodApi = json_decode(postApi(array("barcode" => $barcode),$urlapprove), true);
+
+    if($goodApi['message'] == 'order successfully aproved'){
+        mysqli_query($mysql,"INSERT INTO `zadacha`(`event_id`,`event_date`,`ticket_adult_price`,`ticket_adult_quantity`,`ticket_kid_price`,`ticket_kid_quantity`,`barcode`,`equal_price`) values({$event_id},'{$event_date}',{$ticket_adult_price},{$ticket_adult_quantity},{$ticket_kid_price},{$ticket_kid_quantity},{$barcode},{$equal_price})");
+    } else {
+         echo 'Возникла ошибка: '.$goodApi['error'];  // выводим ряд ошибок для изучения
+    }
+
+} elseif($data['error']  =='barcode already exists'){ // провал пытаемся снова отправить формируя новый уникальный код
+
+$barcodeTwo = mt_rand(111111111111111111,99999999999999999999);
+$postbook = array("event_id" => $event_id, "event_date" => $event_date,"ticket_adult_price" => $ticket_adult_price,"ticket_adult_quantity" => $ticket_adult_quantity,"ticket_kid_price" => $ticket_kid_price,"ticket_kid_quantity" => $ticket_kid_quantity,"barcode" => $barcodeTwo);
+
+$twoShanse = json_decode(postApi($postbook,$urlbook), true);  //делаем по новой
+
+if($twoShanse['message'] == 'order successfully booked'){
+
+    $goodApiTwo = json_decode(postApi(array("barcode" => $barcodeTwo),$urlapprove), true);
+
+    if($goodApiTwo['message'] == 'order successfully aproved'){
+        mysqli_query($mysql,"INSERT INTO `zadacha`(`event_id`,`event_date`,`ticket_adult_price`,`ticket_adult_quantity`,`ticket_kid_price`,`ticket_kid_quantity`,`barcode`,`equal_price`) values({$event_id},'{$event_date}',{$ticket_adult_price},{$ticket_adult_quantity},{$ticket_kid_price},{$ticket_kid_quantity},{$barcode},{$equal_price})");
+    } else {
+         echo 'Возникла ошибка: '.$goodApiTwo['error'];  // выводим ряд ошибок для изучения
+    }
+} else {
+    echo 'Вторая попытка провалена: '.$twoShanse['error'];  // выводим ряд ошибок для изучения
+}
+
+}
+
+}
+
+
+getApp(mt_rand(111,333),date("Y-m-d h:i:s", time()),0,1,0,1);
+?>
